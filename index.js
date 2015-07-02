e1cf125ad5522ead1088eda344e5c8642fd91c25
Ben Drucker <bvdrucker@gmail.com>
2015-07-02 18:05:52 -0400

    Inline object check

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
 'use strict'
 
-var isObject = require('is-object')
-
 module.exports = function isObjectLike (value) {
-  return isObject(value) || typeof value === 'function'
+  return (typeof value === 'object' || typeof value === 'function') && value !== null
 }
